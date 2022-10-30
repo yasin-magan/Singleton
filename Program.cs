03f13ca3407eaaa729a534c24c9237c6bdba7146
YASIN  MAGAN <yasin.aiu@gmail.com>
2022-10-30 13:45:44 +0800

    implement singleton logger

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+// access singleton via static property
+LogManager.Instance.WriteLog("Test Writing");
+Console.ReadKey();
---
+++ Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public  class LogManager
+    {
+
+        private static LogManager _instance;
+
+     public static LogManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new LogManager();
+                   
+                }
+                return _instance;
+            }
+        }
+        private LogManager() // Constructor as Private
+        {
+            string log = "log info";
+             WriteLog(log);
+        }
+
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+        
+    }
+}
